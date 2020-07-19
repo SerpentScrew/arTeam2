8e27d39ac12a5c6f7f8bfe955dc103fdec5796da
hots <hots2650@naver.com>
2020-07-19 16:30:29 +0900

    init Utility

--- app/src/main/java/com/example/arteam2/Activity/MainActivity.java
+++ app/src/main/java/com/example/arteam2/Activity/MainActivity.java
@@ -6,13 +6,14 @@ import android.os.Bundle;
 import android.view.WindowManager;
 
 import com.example.arteam2.R;
+import com.example.arteam2.Utility.FullScreenUtility;
 
 public class MainActivity extends AppCompatActivity {
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		FullScreenUtility.makeNoStatusBar(this);
 		setContentView(R.layout.activity_main);
 	}
 }
--- app/src/main/java/com/example/arteam2/Utility/FullScreenUtility.java
+++ app/src/main/java/com/example/arteam2/Utility/FullScreenUtility.java
@@ -8,11 +8,20 @@ import androidx.appcompat.app.AppCompatActivity;
 public class FullScreenUtility {
 	
 	/**
+	 * 타이틀 제거하는 메뉴
 	 * activity 를 setContentView 하기 전에 사용하면 됨
 	 * @param activity
 	 */
-	public void makeScreenFullSize(AppCompatActivity activity) {
+	public static void makeNoTitle(AppCompatActivity activity) {
 		activity.supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+	}
+	
+	/**
+	 * 상태바 (폰 위의 시커먼거) 제거하는 메뉴
+	 * 마찬가지로 setContentView 하기 전에 사용하면 됨
+	 * @param activity
+	 */
+	public static void makeNoStatusBar(AppCompatActivity activity) {
 		activity.getWindow().setFlags(
 				WindowManager.LayoutParams.FLAG_FULLSCREEN,
 				WindowManager.LayoutParams.FLAG_FULLSCREEN
