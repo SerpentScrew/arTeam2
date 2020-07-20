9205c1ebe00927c902762f58d190e5b023a32c3b
hots <hots2650@naver.com>
2020-07-20 23:08:32 +0900

    MeasureRenderer.java 삭제

--- app/src/main/java/com/example/arteam2/Activity/MeasureActivity.java
+++ app/src/main/java/com/example/arteam2/Activity/MeasureActivity.java
@@ -2,23 +2,46 @@ package com.example.arteam2.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
-import com.example.arteam2.Renderer.MeasureRenderer;
+import com.curvsurf.fsweb.FindSurfaceRequester;
+import com.curvsurf.fsweb.RequestForm;
+import com.curvsurf.fsweb.ResponseForm;
+import com.example.arteam2.Renderer.BackgroundRenderer;
 import com.example.arteam2.R;
+import com.example.arteam2.Renderer.PlaneRenderer;
+import com.example.arteam2.Renderer.PointCloudRenderer;
 import com.example.arteam2.Utility.FullScreenUtility;
 import com.example.arteam2.Utility.PermissionUtility;
 import com.google.ar.core.ArCoreApk;
+import com.google.ar.core.Camera;
 import com.google.ar.core.Config;
+import com.google.ar.core.Frame;
+import com.google.ar.core.Plane;
+import com.google.ar.core.PointCloud;
 import com.google.ar.core.Session;
+import com.google.ar.core.TrackingState;
 import com.google.ar.core.exceptions.CameraNotAvailableException;
 
+import java.io.IOException;
+import java.nio.FloatBuffer;
 import java.util.Objects;
 
-public class MeasureActivity extends AppCompatActivity {
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+public class MeasureActivity extends AppCompatActivity implements GLSurfaceView.Renderer {
+	ScreenStatus screenStatus = new ScreenStatus();
+	
+	private BackgroundRenderer backgroundRenderer = new BackgroundRenderer();
+	private PointCloudRenderer pointCloudRenderer = new PointCloudRenderer();
+	private PlaneRenderer planeRenderer = new PlaneRenderer();
 	
 	private GLSurfaceView measureView;
 	
@@ -26,19 +49,74 @@ public class MeasureActivity extends AppCompatActivity {
 	
 	private boolean userRequestedInstall = false;
 	
-	private MeasureRenderer measureRenderer = new MeasureRenderer(this, new MeasureRenderer.Interface() {
-		@Override
-		public Session getSession() {
-			return session;
+	@Override
+	public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
+		GLES20.glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+		GLES20.glDisable(GLES20.GL_CULL_FACE);
+		GLES20.glEnable(GLES20.GL_DEPTH_TEST);
+		
+		
+		try {
+			backgroundRenderer.whenGLCreate(this);
+			pointCloudRenderer.whenGLCreate(this);
+			planeRenderer.whenGLCreate(this);
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-	});
+	}
+	
+	@Override
+	public void onSurfaceChanged(GL10 gl10, int i, int i1) {
+		screenStatus.isRotated = true;
+		screenStatus.width = i;
+		screenStatus.height = i1;
+		GLES20.glViewport(0, 0, screenStatus.width, screenStatus.height);
+	}
+	
+	@Override
+	public void onDrawFrame(GL10 gl10) {
+		GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
+		
+		if (session == null) return;
+		
+		if (screenStatus.isRotated) {
+			int displayRotation = this.getWindowManager().getDefaultDisplay().getRotation();
+			session.setDisplayGeometry(
+					displayRotation, screenStatus.width, screenStatus.height
+			                          );
+			screenStatus.isRotated = false;
+		}
+		
+		try {
+			session.setCameraTextureName(backgroundRenderer.getTextureId());
+			
+			Frame frame = session.update();
+			backgroundRenderer.draw(frame);
+			
+			Camera camera = frame.getCamera();
+			
+			float[] projMTX = new float[16];
+			camera.getProjectionMatrix(projMTX, 0, 0.1f, 100f);
+			
+			float[] viewMTX = new float[16];
+			camera.getViewMatrix(viewMTX, 0);
+			
+			PointCloud pointCloud = frame.acquirePointCloud();
+			pointCloudRenderer.update(pointCloud);
+			pointCloudRenderer.draw(viewMTX, projMTX);
+			
+		} catch (CameraNotAvailableException e) {
+			e.printStackTrace();
+			finish();
+		}
+	}
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
-//		FullScreenUtility.makeNoStatusBar(this);
-//		FullScreenUtility.makeNoTitle(this);
+		
+		FullScreenUtility.makeNoStatusBar(this);
+		FullScreenUtility.makeNoTitle(this);
 		
 		setContentView(R.layout.activity_measure);
 		
@@ -47,7 +125,7 @@ public class MeasureActivity extends AppCompatActivity {
 		measureView.setPreserveEGLContextOnPause(true);
 		measureView.setEGLContextClientVersion(2);
 		measureView.setEGLConfigChooser(8, 8, 8, 8, 16, 0);
-		measureView.setRenderer(measureRenderer);
+		measureView.setRenderer(this);
 		measureView.setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
 	}
 	
@@ -100,7 +178,61 @@ public class MeasureActivity extends AppCompatActivity {
 	}
 	
 	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		finish();
+	}
+	
+	@Override
 	public void onBackPressed() {
 		super.onBackPressed();
+	}
+
+//	public class PlaneFinder extends AsyncTask<Object, ResponseForm.PlaneParam, ResponseForm.PlaneParam> {
+//		@Override
+//		protected ResponseForm.PlaneParam doInBackground(Object... objects) {
+//			// Ready Point Cloud
+//			FloatBuffer points = pointCloudRenderer.finalPointBuffer;
+//
+//			// Ready Request Form
+//			RequestForm rf = new RequestForm();
+//
+//			rf.setPointBufferDescription(points.capacity() / 4, 16, 0); //pointcount, pointstride, pointoffset
+//			rf.setPointDataDescription(0.05f, 0.01f); //accuracy, meanDistance
+//			rf.setTargetROI(pointCloudRenderer.seedPointID, Math.max(z_dis * circleRad, 0.1f));//seedIndex,touchRadius
+//			rf.setAlgorithmParameter(RequestForm.SearchLevel.NORMAL, RequestForm.SearchLevel.NORMAL);//LatExt, RadExp
+//			Log.d("PointsBuffer", points.toString());
+//			FindSurfaceRequester fsr = new FindSurfaceRequester(REQUEST_URL, true);
+//			// Request Find Surface
+//			try {
+//				Log.d("PlaneFinder", "request");
+//				ResponseForm resp = fsr.request(rf, points);
+//				if (resp != null && resp.isSuccess()) {
+//					ResponseForm.PlaneParam param = resp.getParamAsPlane();
+//					Log.d("PlaneFinder", "request success");
+//					return param;
+//				} else {
+//					Log.d("PlaneFinder", "request fail");
+//				}
+//			} catch (Exception e) {
+//				e.printStackTrace();
+//			}
+//			return null;
+//		}
+//	}
+	
+	private boolean isPlaneExist() {
+		for (Plane plane : session.getAllTrackables(Plane.class)) {
+			if (plane.getTrackingState() == TrackingState.TRACKING
+			    && plane.getSubsumedBy() == null) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	static class ScreenStatus {
+		public boolean isRotated = false;
+		public int width, height;
 	}
 }
